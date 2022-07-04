a7b02ed222f0cf3be20eba14d82fcd25c5ccaea4
GrampaSheev <94834543+GrampaSheev@users.noreply.github.com>
2022-07-04 21:05:59 +0100

    Update graphical.py

--- graphical.py
+++ graphical.py
@@ -26,8 +26,8 @@ def Selection():
 
             cursor.execute("""
             CREATE TABLE IF NOT EXISTS hours (
-                day DATE,
-                hours TEXT
+                day TEXT,
+                hours INT
             )
             """)
 
@@ -44,17 +44,127 @@ def Selection():
     elif type.get() == "Custom":
         Custom=tk.Tk()
         Custom.title("Custom")
-        Custom.geometry("960x540")
+        Custom.geometry("300x150")
+        todayLabel = Label(Custom, text="What date did you work YY/MM/DD")
+        todayEntry = Entry(Custom)
+        todayLabel.pack()
+        todayEntry.pack()
+
+        hoursLabel = Label(Custom, text="How many hours did you work?")
+        hours = Entry(Custom)
+        hoursLabel.pack()
+        hours.pack()
+
+
+        def CustonInput():
+            connection = sql.connect("hours.db")
+
+            cursor = connection.cursor()
+
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS hours (
+                day TEXT,
+                hours INT
+            )
+            """)
+
+            cursor.execute("""
+            INSERT INTO hours 
+            VALUES(?, ?) 
+            """, (todayEntry.get(), hours.get()))
+
+            connection.commit()
+            connection.close()
+        Confirm = Button(Custom, text="Confirm", command=CustonInput)
+        Confirm.pack()
+
 
     elif type.get() == "Viewer":
         Viewer=tk.Tk()
         Viewer.title("Viewer")
         Viewer.geometry("960x540")
+        connection = sql.connect("hours.db")
+
+        cursor = connection.cursor()
+
+        cursor.execute("""
+        SELECT * FROM hours
+        ORDER BY day ASC;
+        """)
+
+        rows = cursor.fetchall()
+
+        connection.commit()
+        connection.close()
+
+
+        ViewerLabel = Label(Viewer, text=rows)
+        ViewerLabel.pack()
+
+        def refresh():
+            Viewer.destroy()
+
+            ViewerR = tk.Tk()
+            ViewerR.title("Viewer")
+            ViewerR.geometry("960x540")
+            connection = sql.connect("hours.db")
+
+            cursor = connection.cursor()
+
+            cursor.execute("""
+                    SELECT * FROM hours
+                    ORDER BY day ASC;
+                    """)
+
+            rows = cursor.fetchall()
+
+            connection.commit()
+            connection.close()
+
+            ViewerLabel = Label(ViewerR, text=rows)
+            ViewerLabel.pack()
+
+            ViewerButton = Button(ViewerR, text="refresh", command=refresh)
+            ViewerButton.pack()
+
+
+
+
+        ViewerButton = Button(Viewer, text="refresh",command=refresh)
+        ViewerButton.pack()
+
+
+    elif type.get() == "Delete":
+
+        Delete = tk.Tk()
+        Delete.title("Delete")
+        Delete.geometry("400x100")
+        dateLabel = Label(Delete, text= "What date was the shift? YY/MM/DD")
+        dateEntry = Entry(Delete)
+        dateLabel.pack()
+        dateEntry.pack()
+
+        def SqlStuffDelete():
+            connection = sql.connect("hours.db")
+            cursor = connection.cursor()
+
+            cursor.execute("""DELETE FROM hours
+                                WHERE day = ?""", (dateEntry.get(),))
+
+            connection.commit()
+            connection.close()
+
+
+        dateButton = Button(Delete,text="Confirm", command=SqlStuffDelete)
+        dateButton.pack()
+
+
+
 
 type = StringVar()
 type.set("Logger")
 
-InitialOption = OptionMenu(window, type, "Logger", "Custom", "Viewer")
+InitialOption = OptionMenu(window, type, "Logger", "Custom", "Viewer", "Delete")
 InitialOption.grid(row=0, column=0)
 
 InitialOptionButton = Button(window, text="Confirm Choice", command=Selection)
